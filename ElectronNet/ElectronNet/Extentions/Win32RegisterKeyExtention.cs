27c4f3fbddf40e7e3cc78b27742db546b4679368
DYH1319 <1245687900@qq.com>
2026-01-19 14:32:53 +0800

    feat: add Windows registry key extension for type-safe value reading

---
+++ ElectronNet/ElectronNet/Extentions/Win32RegisterKeyExtention.cs
@@ -0,0 +1,36 @@
+#if WINDOWS
+
+using System.Runtime.Versioning;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Win32;
+
+public static class Win32RegistryKeyExtension
+{
+    /// <summary>
+    /// 读取注册表值
+    /// </summary>
+    [SupportedOSPlatform("Windows")]
+    public static T? Read<T>(this RegistryKey rk, string name)
+    {
+        var value = rk.GetValue(name)?.ToString();
+        if (value == null) return default;
+
+        if (typeof(T) == typeof(string))
+        {
+            return (T)Convert.ChangeType(value, TypeCode.String);
+        }
+        else if (typeof(T) == typeof(int))
+        {
+            return (T)Convert.ChangeType(value, TypeCode.Int32);
+        }
+        else if (typeof(T) == typeof(long))
+        {
+            return (T)Convert.ChangeType(value, TypeCode.Int64);
+        }
+
+        return default;
+    }
+}
+
+#endif
