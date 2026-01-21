c5ef021f8112ecb508b203c68eaadbf92d349288
DYH1319 <1245687900@qq.com>
2026-01-21 14:11:37 +0800

    feat: add Steam user data synchronization to startup
    - Added DEBUG log prefix constant for enhanced logging capabilities
    - Integrated SteamUserService.SyncDb() into application initialization sequence

--- ElectronNet/ElectronNet/Constants/ConsoleLogPrefix.cs
+++ ElectronNet/ElectronNet/Constants/ConsoleLogPrefix.cs
@@ -2,6 +2,7 @@ namespace ElectronNet.Constants;
 
 public static class ConsoleLogPrefix
 {
+    public const string DEBUG = "[Steam Stat DEBUG]";
     public const string INFO = "[Steam Stat]";
     public const string WARN = "[Steam Stat WARN]";
     public const string ERROR = "[Steam Stat ERROR]";
