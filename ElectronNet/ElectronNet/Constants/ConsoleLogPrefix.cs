5618958620f91a91c5aa3573b0f38a2e6b5b6fd9
DYH1319 <1245687900@qq.com>
2026-01-24 18:35:29 +0800

    feat: add IPC logging prefix constant

--- ElectronNet/ElectronNet/Constants/ConsoleLogPrefix.cs
+++ ElectronNet/ElectronNet/Constants/ConsoleLogPrefix.cs
@@ -10,4 +10,5 @@ public static class ConsoleLogPrefix
     public const string FILE = "[Steam Stat File]";
     public const string SETTING = "[Steam Stat Setting]";
     public const string JOB = "[Steam Stat Job]";
+    public const string IPC = "[Steam Stat IPC]";
 }
